e7e2b8e85e1b38e2af4bb122a73712a1f97893fa
Chris Chu <christopherjc12@gmail.com>
2022-01-04 15:54:06 -0800

    add create function to ticketsCtrl. MVP complete

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -7,9 +7,23 @@ module.exports = {
 }
 
 function newTicket(req, res){
-    res.send('newTicket is being hit')
+    console.log(req.params)
+    res.render('tickets/new', {
+        flightId: req.params.id
+    })
 }
 
 function create(req,res){
     console.log(req.body)
+    console.log(req.params)
+    const newTicket = {
+        flight: req.params.id,
+        seat: req.body.seat,
+        price: req.body.price
+    };
+
+    Ticket.create(newTicket, function(err, ticketDoc){
+        console.log(ticketDoc, "<-- ticket document")
+        res.redirect(`/flights/${ticketDoc.flight}`)
+    })
 }
